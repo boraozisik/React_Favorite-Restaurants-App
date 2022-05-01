81dcb52573ee7ffd9212577168e44be6df27a7cc
Bora Özışık <ozisikbora@gmail.com>
2022-05-01 14:43:53 +0300

    Update FavoriteDialog.js

--- src/components/FavoriteDialog.js
+++ src/components/FavoriteDialog.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Button,Dialog,DialogTitle,DialogContent,DialogContentText,DialogActions } from '@mui/material'
+import { Button,Dialog,DialogTitle,DialogContent,DialogActions } from '@mui/material'
 import { useState } from 'react'
 import FavoriteRestaurantsTable from './FavoriteRestaurantsTable'
-
+import './table.css'
 
 const FavoriteDialog = ({favoriteList}) => {
 
@@ -11,7 +11,7 @@ const FavoriteDialog = ({favoriteList}) => {
 
   return (
     <>
-    <Button   variant="contained" color='success' size='large' onClick={() => setOpen(true)} style={{marginLeft: 950, width:300}}>Show Favorites</Button>
+    <Button className='favoriteDialog'  variant="contained" color='success' size='small' onClick={() => setOpen(true)} style={{width:170,color: '#283747',backgroundColor:'#F1948A',fontSize:14}}>Show Favorites</Button>
     <Dialog aria-labelledby='dialog-title' aria-describedby='dialog-description' open={open} onClose={() => setOpen(false)}>
         <DialogTitle id='dialog-title'>
             Your Favorite Restaurants
@@ -21,7 +21,7 @@ const FavoriteDialog = ({favoriteList}) => {
         </DialogContent>
         <DialogActions>
             <Button onClick={() => setOpen(false)}>Exit</Button>
-            <Button autoFocus onClick={() => console.log(favoriteList)}>Save</Button>
+            
         </DialogActions>
     </Dialog>
     </>
